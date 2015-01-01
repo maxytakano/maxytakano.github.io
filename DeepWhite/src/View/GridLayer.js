422d73f7c4954e645a1ee432fabd20a464401201
Maximilien Takano <maxytakano@gmail.com>
2015-01-01 00:08:25 -0800

    ho

--- DeepWhite/src/View/GridLayer.js
+++ DeepWhite/src/View/GridLayer.js
@@ -13,19 +13,19 @@ var GridLayer = cc.Layer.extend({
         var winSize = cc.director.getWinSize();
         var localSize = (winSize.width * 0.75);
 
-        //var draw = new cc.DrawNode();
-        //this.addChild(draw);
-        //
-        //// Divide the screen into board size + 2 tiles
-        //this.tileSize = localSize / (boardSize + 2);
-        //
-        //for (var x = 0; x < boardSize; x++) {
-        //    draw.drawSegment(
-        //        cc.p((this.tileSize * x) + (this.tileSize * 1.5), (this.tileSize * 1.5)),
-        //        cc.p((this.tileSize * x) + (this.tileSize * 1.5), localSize - (this.tileSize * 1.5)),
-        //        1.1, cc.color(0, 0, 0, 255)
-        //    );
-        //}
+        var draw = new cc.DrawNode();
+        this.addChild(draw);
+
+        // Divide the screen into board size + 2 tiles
+        this.tileSize = localSize / (boardSize + 2);
+
+        for (var x = 0; x < boardSize; x++) {
+            draw.drawSegment(
+                cc.p((this.tileSize * x) + (this.tileSize * 1.5), (this.tileSize * 1.5)),
+                cc.p((this.tileSize * x) + (this.tileSize * 1.5), localSize - (this.tileSize * 1.5)),
+                1.1, cc.color(0, 0, 0, 255)
+            );
+        }
         //
         //for (var y = 0; y < boardSize; y++) {
         //    draw.drawSegment(
